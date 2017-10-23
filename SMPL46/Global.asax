95fc27f3b28b5cff7b55e5ad80d10aaeeb4a0928
turnerm <mark.turner@ibasesystems.com>
2017-10-23 11:36:14 +0100

    Add project files.

---
+++ SMPL46/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMPL46.MvcApplication" Language="C#" %>
---
+++ SMPL46/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMPL46.Startup))]
+namespace SMPL46
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
